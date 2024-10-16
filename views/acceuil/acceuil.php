884af8747870010a9c3b175f1bf2ea17ae6851df
michelint <lemoderateurdelive@gmail.com>
2024-10-16 09:11:39 +0200

    Maj - Premier pas de l'interface graphique

---
+++ views/acceuil/acceuil.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Acceuil</title>
+</head>
+<body>
+<div class="jumbotron text-center my-4 mt-5 mb-5">
+    <h3 class="text text-secondary">📚​ Bienvenue dans mon site de User Story 📚​</h3>
+    <a href="index.php?route=creercompte" class="btn btn-danger mt-3">Créer un compte</a>
+</div>
+</body>
+</html>
